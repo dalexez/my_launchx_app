110c6e3e98db853ef1450fdfbe4a003d6addde31
DESKTOP-OGVM5PE\da_le <dalexez@gmail.com>
2022-04-22 11:26:51 -0500

    Paso 3: Cracion de una clase y modularizacion

---
+++ index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./app/missionCommander')
+
+const carlo = new MissionCommander("Carlo")
+const rodrigo = new MissionCommander("Rodrigo")
+const fer = new MissionCommander("Fer")
+
+console.log(carlo.name)
+console.log(rodrigo.name)
+console.log(fer.name)
